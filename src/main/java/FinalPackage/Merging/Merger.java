16e3b10d341be071f2129fb2236688c067e5215d
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-08-02 12:45:52 +0200

    Added Comments to Merger

--- src/main/java/FinalPackage/Merging/Merger.java
+++ src/main/java/FinalPackage/Merging/Merger.java
@@ -19,13 +19,30 @@ import static FinalPackage.Merging.FunctionalPropertyDetector.isFunctionalProper
 
 public class Merger {
 
-    // Method to merge multiple knowledge graphs from a list of RDF models
+    /*
+        Method to merge multiple knowledge graphs from a list of RDF models
+        Target Model is the merged model which we want to return
+        Resolved Model is a helping model, it contains all subject, predicate combination, where we checked for the right object already
+
+        Iterate over each model and merge its properties
+        1.  Check if the subject-predicate combination has been resolved before
+        2.  Check if the Subject-Predicate combination exists in the targetModel
+        3.  Remove the old statements (if any) from the targetModel
+        4.  Check in all models if predicate is functional (for-loop).
+        5.  If predicate is not functional for every single model
+            -> property cant be functional:
+                 Add all statements with this subject predicate combination to the target model
+                 and add one statement to resolvedModel (-> subject, predicate have been checked)
+        6.  If it is functional in all models
+            -> we assume its functional
+            -> only one object for subject predicate combination allowed
+            -> Call Function to resolve: Only one valid object for subject, predicate combination
+     */
     public static Model mergeGraphs(List<Model> models) {
         Model targetModel = ModelFactory.createDefaultModel();
         Model resolvedModel = ModelFactory.createDefaultModel();
         boolean functionalProperty = true;
 
-        // Iterate over each model and merge its properties
         for (Model model : models) {
             StmtIterator iter = model.listStatements();
             while (iter.hasNext()) {
@@ -33,16 +50,11 @@ public class Merger {
                 Resource subject = stmt.getSubject();
                 Property predicate = stmt.getPredicate();
 
-                // Check if the subject-predicate combination has been resolved before
                 if (!resolvedModel.contains(subject, predicate)) {
-                    // Check if the Subject-Predicate combination exists in the targetModel
                     if (!targetModel.contains(subject, predicate)) {
                         targetModel.add(stmt);
                     } else {
-                        // Remove the old statements (if any) from the targetModel
                         targetModel.removeAll(subject, predicate, null);
-
-                        //Check if functional. If not for every single model: Add all statements with this subject predicate comnbination
                         for (Model model1 : models) {
                             functionalProperty = isFunctionalProperty(model1, predicate);
                             if (!functionalProperty){
@@ -53,13 +65,11 @@ public class Merger {
                                 break;
                             }
                         }
-
                         if (functionalProperty){
-                            // Call a method to find the resolved value and add it to the targetModel
                             System.out.println("Functional: ");
+                            // Call a method to find the resolved value and add it to the targetModel
                             RDFNode resolvedObject = getResolvedObjectValue(models, subject, predicate);
                             targetModel.add(subject, predicate, resolvedObject);
-                            // Mark the statement as resolved by adding it to the resolvedModel
                             resolvedModel.add(stmt);
                         }
                     }
@@ -68,22 +78,20 @@ public class Merger {
                 functionalProperty = true;
             }
         }
-
         return targetModel;
     }
 
-    // Method to find the resolved value for a subject-predicate combination from multiple models
+
+    /*
+        Method to find the resolved value for a subject-predicate combination from multiple models
+        for loop: go through statements with given subject-predicate combination
+            -> if it is the first, assigned it to commonObject
+            -> if not check for every statement if it is the same until we find one that isn't
+                -> call method to resolve for one object and exit loop
+     */
     public static RDFNode getResolvedObjectValue(List<Model> models, Resource subject, Property predicate) {
         RDFNode commonObject = null;
-        boolean hasConflict = false;
-        RDFNode dummyObject = null;
-
-        for (Model model : models) {
-            StmtIterator iter = model.listStatements(subject, predicate, (RDFNode) null);
-            if (iter.hasNext()) {
-                dummyObject = iter.nextStatement().getObject();
-            }
-        }
+        boolean conflict = false;
 
         for (Model model : models) {
             StmtIterator iter = model.listStatements(subject, predicate, (RDFNode) null);
@@ -92,28 +100,28 @@ public class Merger {
                 if (commonObject == null) {
                     commonObject = object;
                 } else if (!commonObject.equals(object)) {
-                    hasConflict = true;
+                    conflict = true;
                     Set<RDFNode> allObjects = getAllObjects(models, subject, predicate);
                     //todo: Resolve for all objects
 
-                    System.out.println("Conflict: "+ subject + predicate);
+                    System.out.println("Conflict: " + subject + predicate);
                     for (RDFNode object1 : allObjects) {
                         System.out.println(object1.toString());
                     }
                     break;
-                } else {
-                    System.out.println("Dummy: " + subject + predicate);
-                    Set<RDFNode> allObjects = getAllObjects(models, subject, predicate);
-                    for (RDFNode object1 : allObjects) {
-                        System.out.println(object1.toString());
-                    }
                 }
             }
         }
-
-        return hasConflict ? dummyObject : commonObject;
+        return commonObject;
+        //at the moment we just return the first entry until we find a conflict
+        //when we implement solution we need tio return this or return directly in loop
+        //return conflict ? resolvedObject : commonObject;
     }
 
+
+    /*
+        Get a Model which contains all distinct statements from all the models with a given subject and predicate
+     */
     public static Model getDistinctStatements(List<Model> models, Resource subject, Property predicate) {
         Model distinctStatements = ModelFactory.createDefaultModel();
 
@@ -128,6 +136,10 @@ public class Merger {
         return distinctStatements;
     }
 
+
+    /*
+        Get a set of all objects in a list of models which appear in a property with a given subject and predicate
+     */
     public static Set<RDFNode> getAllObjects(List<Model> models, Resource subject, Property predicate) {
         Set<RDFNode> objects = new HashSet<>();
 
@@ -141,91 +153,5 @@ public class Merger {
 
         return objects;
     }
-
-
-    public static Model mergeGraphs(Model model1, Model model2){
-        Model targetModel = ModelFactory.createDefaultModel();
-
-        // Merge properties from model1
-        StmtIterator iter = model1.listStatements();
-        while (iter.hasNext()) {
-            Statement stmt = iter.nextStatement();
-            Resource subject = stmt.getSubject();
-            Property predicate = stmt.getPredicate();
-            RDFNode object = stmt.getObject();
-
-            // Check if the Subject-Predicate combination exists in model2
-            if (!model2.contains(subject, predicate)) {
-                targetModel.add(stmt);
-            } else {
-                // Call a method to check if the objects are the same and handle conflict
-                Statement stmt2 = model2.getProperty(subject, predicate);
-                RDFNode object2 = stmt2.getObject();
-                if (object.equals(object2)) {
-                    targetModel.add(stmt);
-                } else {
-                    resolveConflict(subject, predicate, object, object2);
-                }
-            }
-        }
-
-        // Merge properties from model2 that are not in model1
-        iter = model2.listStatements();
-        while (iter.hasNext()) {
-            Statement stmt = iter.nextStatement();
-            Resource subject = stmt.getSubject();
-            Property predicate = stmt.getPredicate();
-            RDFNode object = stmt.getObject();
-
-            // Check if the Subject-Predicate combination exists in model1
-            if (!model1.contains(subject, predicate)) {
-                targetModel.add(stmt);
-            }
-        }
-
-        return targetModel;
-    }
-
-    // Method to resolve conflicts when the same subject and predicate combination have different objects in both graphs
-    private static void resolveConflict(Resource subject, Property predicate, RDFNode object1, RDFNode object2) {
-
-        fusionStrategy fusionStrategy = new fusionStrategy();
-        //Both objects are resources -> Both objects are literals -> one is a literal, and one is a resource
-        if (object1.isResource() && object2.isResource()) {
-            // Handle conflict between two resources
-            Resource resource1 = object1.asResource();
-            Resource resource2 = object2.asResource();
-            // Your resource conflict resolution logic here.
-            fusionStrategy.fuseResources(resource1, resource2);
-
-            System.out.println("Conflict detected for resources: " + subject + " " + predicate);
-            System.out.println("Resource in model1: " + resource1);
-            System.out.println("Resource in model2: " + resource2);
-        } else if (object1.isLiteral() && object2.isLiteral()) {
-            // Handle conflict between two literals
-            Literal literal1 = object1.asLiteral();
-            Literal literal2 = object2.asLiteral();
-            // Your literal conflict resolution logic here.
-            //Here are the code that take the two literals and fuse them
-            fusionStrategy.fuseLiterals(literal1, literal2);
-
-            System.out.println("Conflict detected for literals: " + subject + " " + predicate);
-            System.out.println("Literal in model1: " + literal1);
-            System.out.println("Literal in model2: " + literal2);
-        } else {
-            // Handle conflict between a resource and a literal
-            Resource resource = object1.isResource() ? object1.asResource() : object2.asResource();
-            Literal literal = object1.isLiteral() ? object1.asLiteral() : object2.asLiteral();
-            // Your resource-literal conflict resolution logic here.
-            fusionStrategy.fuseResourceLiteral(resource, literal);
-
-            System.out.println("Conflict detected for resource-literal: " + subject + " " + predicate);
-            System.out.println("Resource in model: " + resource);
-            System.out.println("Literal in model: " + literal);
-        }
-
-        // You can modify this method to suit your specific conflict resolution requirements.
-    }
-
 
 }
