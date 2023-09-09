18a7d9ce4c385a20c9125961850fbfbf7668e1ef
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-09-09 17:34:01 +0200

    FunctionalPropertyFinder reworked

--- src/main/java/FinalPackage/Merging/FunctionalPropertyFinder.java
+++ src/main/java/FinalPackage/Merging/FunctionalPropertyFinder.java
@@ -6,47 +6,36 @@ import java.util.*;
 
 public class FunctionalPropertyFinder {
     // Define the threshold percentage (e.g., 90%)
-    private static final double THRESHOLD_PERCENTAGE = 0.1;
+    private static final double THRESHOLD_PERCENTAGE = 0.25;
 
     public static List<Property> findFunctionalProperties(List<Model> models) {
-        List<Property> functionalProperties = new ArrayList<>();
+        // Count the occurrences of each predicate in all the combined models
+        Map<Property, Integer> predicateCounts = new HashMap<>();
 
-        // Iterate over each model
         for (Model model : models) {
-            // Count the occurrences of each predicate in the model
-            Map<Property, Integer> predicateCounts = new HashMap<>();
             StmtIterator stmtIterator = model.listStatements();
             while (stmtIterator.hasNext()) {
                 Statement statement = stmtIterator.next();
                 Property predicate = statement.getPredicate();
                 predicateCounts.put(predicate, predicateCounts.getOrDefault(predicate, 0) + 1);
             }
+        }
 
-            // Iterate over all predicates and check if they are functional using SPARQL
-            for (Property predicate : predicateCounts.keySet()) {
-                int totalOccurrences = predicateCounts.get(predicate);
-                if (isFunctionalInModel(predicate, model)) {
-                    // Check if the functional occurrences exceed the threshold percentage
-                    int functionalOccurrences = countFunctionalOccurrences(predicate, model);
-                    double percentage = (double) functionalOccurrences / totalOccurrences;
-                    if ((1 - percentage) <= THRESHOLD_PERCENTAGE) {
-                        functionalProperties.add(predicate);
-                    }
-                }
+        List<Property> functionalProperties = new ArrayList<>();
+
+        for (Property predicate : predicateCounts.keySet()) {
+            int totalOccurrences = predicateCounts.get(predicate);
+            int totalFunctionalOccurrences = 0;
+
+            // Calculate the total functional occurrences across all models
+            for (Model model : models) {
+                totalFunctionalOccurrences += countFunctionalOccurrences(predicate, model);
             }
-        }
+            //System.out.println("Property: " + predicate + " total: " + totalOccurrences + " func: " + totalFunctionalOccurrences);
 
-        // Filter the functional properties based on the threshold
-        Map<Property, Integer> functionalCounts = new HashMap<>();
-        for (Property property : functionalProperties) {
-            functionalCounts.put(property, functionalCounts.getOrDefault(property, 0) + 1);
-        }
-
-        functionalProperties.clear(); // Clear the list and re-add based on the threshold
-        for (Map.Entry<Property, Integer> entry : functionalCounts.entrySet()) {
-            double percentage = (double) entry.getValue() / models.size();
-            if ((1 - percentage) <= THRESHOLD_PERCENTAGE) {
-                functionalProperties.add(entry.getKey());
+            double percentage = (double) totalFunctionalOccurrences / totalOccurrences;
+            if (percentage >= 1 - THRESHOLD_PERCENTAGE) {
+                functionalProperties.add(predicate);
             }
         }
 
@@ -76,3 +65,48 @@ public class FunctionalPropertyFinder {
         }
     }
 }
+
+
+//Old!
+        /*List<Property> functionalProperties = new ArrayList<>();
+
+        // Iterate over each model
+        for (Model model : models) {
+            // Count the occurrences of each predicate in the model
+            Map<Property, Integer> predicateCounts = new HashMap<>();
+            StmtIterator stmtIterator = model.listStatements();
+            while (stmtIterator.hasNext()) {
+                Statement statement = stmtIterator.next();
+                Property predicate = statement.getPredicate();
+                predicateCounts.put(predicate, predicateCounts.getOrDefault(predicate, 0) + 1);
+            }
+
+            // Iterate over all predicates and check if they are functional using SPARQL
+            for (Property predicate : predicateCounts.keySet()) {
+                int totalOccurrences = predicateCounts.get(predicate);
+                if (isFunctionalInModel(predicate, model)) {
+                    // Check if the functional occurrences exceed the threshold percentage
+                    int functionalOccurrences = countFunctionalOccurrences(predicate, model);
+                    double percentage = (double) functionalOccurrences / totalOccurrences;
+                    if (percentage >= 1 - THRESHOLD_PERCENTAGE) {
+                        functionalProperties.add(predicate);
+                    }
+                }
+            }
+        }
+
+        // Filter the functional properties based on the threshold
+        Map<Property, Integer> functionalCounts = new HashMap<>();
+        for (Property property : functionalProperties) {
+            functionalCounts.put(property, functionalCounts.getOrDefault(property, 0) + 1);
+        }
+
+        functionalProperties.clear(); // Clear the list and re-add based on the threshold
+        for (Map.Entry<Property, Integer> entry : functionalCounts.entrySet()) {
+            double percentage = (double) entry.getValue() / models.size();
+            if (percentage >= 1 - THRESHOLD_PERCENTAGE) {
+                functionalProperties.add(entry.getKey());
+            }
+        }
+
+        return functionalProperties;*/
