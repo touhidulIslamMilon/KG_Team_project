58ead78172344384b636f7eb9eee5f6a34a22107
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-09-22 11:02:12 +0200

    Reformatting/ Warnings in ConflictResolution

--- src/main/java/FinalPackage/Merging/ConflictResolution.java
+++ src/main/java/FinalPackage/Merging/ConflictResolution.java
@@ -7,8 +7,6 @@ import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 
-import java.util.Map;
-
 public class ConflictResolution {
 
     /*
