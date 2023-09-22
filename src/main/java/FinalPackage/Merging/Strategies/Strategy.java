ba2cb293187d047ff0e92687fa90ee65d84a1faf
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-09-22 11:02:51 +0200

    Reformatting/ Warnings in Strategy

--- src/main/java/FinalPackage/Merging/Strategies/Strategy.java
+++ src/main/java/FinalPackage/Merging/Strategies/Strategy.java
@@ -1,12 +1,9 @@
 package FinalPackage.Merging.Strategies;
 
+import com.google.common.collect.ListMultimap;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
-
-import com.google.common.collect.ListMultimap;
-
-import java.util.Map;
 
 interface Strategy {
     RDFNode resolveConflict(ListMultimap<RDFNode, Integer> objects, Resource subject, Property predicate);
