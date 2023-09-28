c6af11c2eadc0bd73368d1e1a931d0172396f5e3
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-09-28 15:36:22 +0200

    Added Test for Merger for all Test Cases

---
+++ src/main/java/FinalPackage/analyzeGraph/ConflictFinder.java
@@ -0,0 +1,71 @@
+package FinalPackage.analyzeGraph;
+
+
+import lombok.Getter;
+import org.apache.jena.rdf.model.*;
+
+import java.util.*;
+
+public class ConflictFinder {
+
+    public static Map<SubjectPredicatePair, List<RDFNode>> findConflictingObjects(List<Model> models) {
+        Map<SubjectPredicatePair, List<RDFNode>> conflictMap = new HashMap<>();
+
+        for (Model model : models) {
+            StmtIterator stmtIterator = model.listStatements();
+
+            while (stmtIterator.hasNext()) {
+                Statement statement = stmtIterator.nextStatement();
+                SubjectPredicatePair pair = new SubjectPredicatePair(statement.getSubject(), statement.getPredicate());
+                RDFNode object = statement.getObject();
+
+                // Check if the pair is already in the conflictMap
+                if (conflictMap.containsKey(pair)) {
+                    List<RDFNode> objects = conflictMap.get(pair);
+
+                    // Check if the object is not already in the list of conflicting objects
+                    if (!objects.contains(object)) {
+                        objects.add(object);
+                    }
+                } else {
+                    // If the pair is not in the map, add it with a new list containing the object
+                    List<RDFNode> objects = new ArrayList<>();
+                    objects.add(object);
+                    conflictMap.put(pair, objects);
+                }
+            }
+
+            // Clear the model to release memory
+            model.close();
+        }
+
+        // Remove pairs with only one object (no conflict)
+        conflictMap.entrySet().removeIf(entry -> entry.getValue().size() <= 1);
+
+        return conflictMap;
+    }
+
+    @Getter
+    public static class SubjectPredicatePair {
+        private final Resource subject;
+        private final Property predicate;
+
+        public SubjectPredicatePair(Resource subject, Property predicate) {
+            this.subject = subject;
+            this.predicate = predicate;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(subject, predicate);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            SubjectPredicatePair other = (SubjectPredicatePair) obj;
+            return Objects.equals(subject, other.subject) && Objects.equals(predicate, other.predicate);
+        }
+    }
+}
---
+++ src/test/java/tests/ConflictFinderTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import FinalPackage.analyzeGraph.ConflictFinder;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ConflictFinderTest {
+
+    public static void main(String[] args) {
+        // Create a list of models from RDF files (you can replace this with your own data loading logic)
+        //List<Model> models = LoadRDF.readAllTestCases();
+
+        Model model1 = ModelFactory.createDefaultModel();
+        model1.read("src/test/testResources/starwars1Test.rdf");
+
+        Model model2 = ModelFactory.createDefaultModel();
+        model2.read("src/test/testResources/starwars2Test.rdf");
+
+        Model model3 = ModelFactory.createDefaultModel();
+        model3.read("src/test/testResources/starwars3Test.rdf");
+
+        List<Model> models = new ArrayList<>();
+        models.add(model1);
+        models.add(model2);
+        models.add(model3);
+
+        System.out.println("Models loaded");
+
+        // Find conflicting statements
+        Map<ConflictFinder.SubjectPredicatePair, List<RDFNode>> conflicts = ConflictFinder.findConflictingObjects(models);
+        System.out.println("Model created");
+
+        // Print the conflicts
+        for (Map.Entry<ConflictFinder.SubjectPredicatePair, List<RDFNode>> entry : conflicts.entrySet()) {
+            System.out.println("\nSubject: " + entry.getKey().getSubject() + "\nPredicate: " + entry.getKey().getPredicate() + "\nConflicting Objects: " + entry.getValue());
+        }
+
+    }
+
+}
