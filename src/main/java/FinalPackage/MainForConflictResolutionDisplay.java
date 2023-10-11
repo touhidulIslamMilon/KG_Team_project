33cf027172022c190e69b79b100346aabf3ace0a
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2023-10-11 21:50:19 +0200

    Typos in MainForConflictResolutionDisplay

--- src/main/java/FinalPackage/MainForConflictResolutionDisplay.java
+++ src/main/java/FinalPackage/MainForConflictResolutionDisplay.java
@@ -13,7 +13,7 @@ public class MainForConflictResolutionDisplay {
         Model mergedModel = Merger.mergeGraphs(models);
 
         System.out.println("For string conflict");
-        retrieveStatementsForSubjectPredicate(mergedModel,"http://dbkwik.webdatacommons.org/marvel.wikia.com/resource/Category:Kevin_McTaggart_(Earth-10005)/Appearances", "http://www.w.org/2000/01/rdf-schema#label");
+        retrieveStatementsForSubjectPredicate(mergedModel,"http://dbkwik.webdatacommons.org/marvel.wikia.com/resource/Category:Kevin_McTaggart_(Earth-10005)/Appearances", "http://www.w3.org/2000/01/rdf-schema#label");
 
         System.out.println("For integer conflict");
         retrieveStatementsForSubjectPredicate(mergedModel, "http://dbkwik.webdatacommons.org/memory-beta.wikia.com/resource/Kopman", "http://dbkwik.webdatacommons.org/memory-beta.wikia.com/property/insignia");
@@ -22,7 +22,7 @@ public class MainForConflictResolutionDisplay {
         retrieveStatementsForSubjectPredicate(mergedModel, "http://dbkwik.webdatacommons.org/memory-beta.wikia.com/resource/2400s","http://dbkwik.webdatacommons.org/memory-beta.wikia.com/property/dp");
 
         System.out.println("For date conflict");
-        retrieveStatementsForSubjectPredicate(mergedModel, "http://dbkwik.webdatacommons.org/stexpanded.wikia.com/resource/When_It_Rains...","http://dbkwik.webdatacommons.orq/stexpanded.wikia.com/property/originaldate");
+        retrieveStatementsForSubjectPredicate(mergedModel, "http://dbkwik.webdatacommons.org/stexpanded.wikia.com/resource/When_It_Rains...","http://dbkwik.webdatacommons.org/stexpanded.wikia.com/property/originaldate");
 
         System.out.println("For URI conflict");
         retrieveStatementsForSubjectPredicate(mergedModel, "http://dbkwik.webdatacommons.org/memory-beta.wikia.com/resource/D%27deridex_class","http://dbkwik.webdatacommons.org/memory-beta.wikia.com/property/servicePeriod");
